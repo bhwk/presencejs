b8de5a7b130f0bce00b1f653e2c42ec283180522
bhwk <120708160+bhwk@users.noreply.github.com>
2023-02-23 01:09:20 +0800

    User validation placeholder

--- src/commands/registerWatcher.js
+++ src/commands/registerWatcher.js
@@ -1,24 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { user_ID } = require('..');
 
-console.log(user_ID)
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('register')
         .setDescription('Registers user to watcher list'),
     async execute(interaction) {
         const id = interaction.user.id;
+
         if (user_ID.includes(id)) {
             await interaction.reply({
                 content: `${interaction.user} is already registered`,
                 ephemeral: true
             })
         } else {
+            user_ID.push(id);
             await interaction.reply({
                 content: `${interaction.user} has been added to watch list`,
                 ephemeral:true
             })
         }
+        console.log(user_ID)
     }
 }
--- src/commands/unregisterWatcher.js
+++ src/commands/unregisterWatcher.js
@@ -1,14 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder } = require('discord.js');
+const { user_ID } = require('..');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('unregister')
         .setDescription('Removes user from watch list'),
     async execute(interaction) {
-        const user = interaction.user
-        await interaction.reply({
-            content: `${user} has been removed from watch list`,
-            ephemeral:true
-        })
+        const id = interaction.user.id;
+
+        if(user_ID.includes(id)) {
+            index = user_ID.indexOf(id);
+            user_ID.splice(index,1);
+            await interaction.reply({
+                content: `${interaction.user} has been removed from watch list`,
+                ephemeral:true
+            });
+        } else {
+            await interaction.reply({
+                content:`${interaction.user} is not on the watch list`,
+                ephemeral:true
+            });
+        }
+        console.log(user_ID)
     }
 }
