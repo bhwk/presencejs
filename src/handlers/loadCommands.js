6e9ff4eb523e7c19272f46f730fa5d0cd00d7e6e
bhwk <120708160+bhwk@users.noreply.github.com>
2023-02-22 20:00:19 +0800

    Formatting, additional logging

--- src/handlers/loadCommands.js
+++ src/handlers/loadCommands.js
@@ -1,39 +1,46 @@
 // const command = require(`../commands/${command}`)
 function loadCommands(client) {
-    const fs = require('node:fs')
-    const path = require('node:path')
+    const fs = require("node:fs");
+    const path = require("node:path");
 
-    const commandsPath = path.resolve('src/', 'commands') 
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandsPath = path.resolve("src/", "commands");
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
 
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
         // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
+        if ("data" in command && "execute" in command) {
             client.commands.set(command.data.name, command);
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
         }
     }
 
-    client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
-    if(!command) {
-        console.error(`No command matching ${interaction.commandName}`)
-        return;
-    }
-    try{ 
-        console.log(`[${interaction.commandName}] called by ${interaction.user}`);
-        await command.execute(interaction);
-    } catch {
-        console.error(error);
-        await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true})
-    }
-})
+    client.on("interactionCreate", async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+        const command = interaction.client.commands.get(interaction.commandName);
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName}`);
+            return;
+        }
+        try {
+            console.log(`[${interaction.commandName}] called by ${interaction.user}, ${interaction.user.username}`);
+            await command.execute(interaction);
+        } catch {
+            console.error(error);
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                ephemeral: true,
+            });
+        }
+    });
 }
 
 module.exports = {
     loadCommands,
-} 
+};
