bc6ab7526a2a93f9c3135fd18689f1a4b68c26be
bhwk <120708160+bhwk@users.noreply.github.com>
2023-02-22 19:22:42 +0800

    Modify index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-// Require the necessary discord.js classes
 require('dotenv').config()
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { loadEvents} = require('./handlers/loadEvents')
+const fs = require('node:fs')
+const path = require('node:path')
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { loadEvents} = require('./handlers/loadEvents');
+const { loadCommands } = require('./handlers/loadCommands');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences] })
 
+client.commands = new Collection();
+
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Ready! Logged in as ${c.user.tag}`)
 });
+
 
 client.login(process.env.TOKEN)
 loadEvents(client)
+loadCommands(client)
