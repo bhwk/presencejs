71d8b301e87f2929a67fe7116cdf6d9b48adb6fc
bhwk <120708160+bhwk@users.noreply.github.com>
2023-02-23 23:38:58 +0800

    Get request for discord id

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ const { connect } = require('mongoose');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const { loadEvents } = require('./handlers/loadEvents');
 const { loadCommands } = require('./handlers/loadCommands');
+const Presence = require('./schemas/presence');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/presence/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    let presenceProfile = await Presence.findOne({
+        userID: id,
+    });
+    console.log(presenceProfile);
+    if (!presenceProfile) {
+        res.status(418).send({ message: 'User ID does not exist!' });
+    } else {
+        res.status(200).send({ presenceProfile });
+    }
+});
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildPresences],
@@ -12,6 +30,11 @@ client.commands = new Collection();
 
 client.once(Events.ClientReady, (c) => {
     console.log(`[Ready] Logged in as ${c.user.tag}`);
+
+    app.use(express.json());
+    app.listen(PORT, () => {
+        console.log(`[EXPRESS] Listening on PORT:${PORT}`);
+    });
 });
 
 loadEvents(client);
