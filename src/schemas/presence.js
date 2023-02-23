d608da65d8f2dcd9819a5352a37760eb2c97346d
bhwk <120708160+bhwk@users.noreply.github.com>
2023-02-23 16:33:14 +0800

    Add activity to schema

--- src/schemas/presence.js
+++ src/schemas/presence.js
@@ -1,7 +1,8 @@
 const { Schema, model } = require('mongoose');
 const presenceSchema = new Schema({
-	_id: Schema.Types.ObjectId,
-	userID: String,
+    _id: Schema.Types.ObjectId,
+    userID: String,
+    activity: { type: String, required: false },
 });
 
 module.exports = model('Presence', presenceSchema, 'Users');
