56ce9043146600549bd2593c5fb2389b1afbaf63
bhwk <120708160+bhwk@users.noreply.github.com>
2023-02-23 19:48:20 +0800

    Refactor event handling

--- src/handlers/loadEvents.js
+++ src/handlers/loadEvents.js
@@ -1,17 +1,37 @@
-const event = (event) => require(`../events/${event}`);
+const event = (event) => require(`../events/client/${event}`);
 const mongoEvent = (mongoEvent) =>
     require(`../events/mongoEvents/${mongoEvent}`);
 const { connection } = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+const eventsPath = path.resolve('src/events', 'client');
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter((file) => file.endsWith('.js'));
+
+const mongoPath = path.resolve('src/events', 'mongoEvents');
+const mongoFiles = fs
+    .readdirSync(mongoPath)
+    .filter((file) => file.endsWith('.js'));
 
 function loadEvents(client) {
-    client.on('presenceUpdate', event('presenceUpdate').execute);
-    client.on('interactionCreate', event('interactionCreate').execute);
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        client.on(event.name, (...args) => {
+            event.execute(...args);
+            console.log(event.name);
+        });
+    }
 
-    //mongoDB events
-    connection.once('connected', mongoEvent('connected').execute);
-    connection.on('connecting', mongoEvent('connecting').execute);
-    connection.on('disconnected', mongoEvent('disconnected').execute);
-    connection.on('err', mongoEvent('err').execute);
+    for (const file of mongoFiles) {
+        const filePath = path.join(mongoPath, file);
+        const mongoEvent = require(filePath);
+        connection.on(mongoEvent.name, (...args) =>
+            mongoEvent.execute(...args)
+        );
+    }
 }
 
 module.exports = {
